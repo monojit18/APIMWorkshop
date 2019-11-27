88a83ce1f94519c34170739d8e961aacfe4a0a60
monojit18 <datta.monojit@gmail.com>
2019-11-27 19:20:32 +0530

    Initial commit

---
+++ Scripts/apim-deploy.ps1
@@ -0,0 +1,3 @@
+Test-AzResourceGroupDeployment -ResourceGroupName "workshopsgroups" `
+-TemplateFile "../Standard/apim-master-deploy.json" `
+-TemplateParameterFile "../Standard/apim-master-deploy.parameters.json"
