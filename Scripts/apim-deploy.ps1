130cd1b5e1998cb978cd1b914db3b2a70ed4d8e2
monojit18 <datta.monojit@gmail.com>
2019-11-29 07:10:16 +0530

    Added all sub components

--- Scripts/apim-deploy.ps1
+++
@@ -1,3 +0,0 @@
-Test-AzResourceGroupDeployment -ResourceGroupName "workshopsgroups" `
--TemplateFile "../Standard/apim-master-deploy.json" `
--TemplateParameterFile "../Standard/apim-master-deploy.parameters.json"
