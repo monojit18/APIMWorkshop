19c35c4d7bc6df090455f103a3d52f48950a96d0
monojit18 <datta.monojit@gmail.com>
2019-12-06 15:46:40 +0530

    Version Set missing

--- Scripts/apim-basic-deploy.ps1
+++ Scripts/apim-basic-deploy.ps1
@@ -2,6 +2,6 @@ Test-AzResourceGroupDeployment -ResourceGroupName "workshopsgroups" `
 -TemplateFile "../Basic/apim-master-deploy.json" `
 -TemplateParameterFile "../Basic/apim-master-deploy.parameters.json"
 
-New-AzResourceGroupDeployment -ResourceGroupName "workshopsgroups" `
--TemplateFile "../Basic/apim-master-deploy.json" `
--TemplateParameterFile "../Basic/apim-master-deploy.parameters.json"
+# New-AzResourceGroupDeployment -ResourceGroupName "workshopsgroups" `
+# -TemplateFile "../Basic/apim-master-deploy.json" `
+# -TemplateParameterFile "../Basic/apim-master-deploy.parameters.json"
